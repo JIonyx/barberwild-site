7a6ae6aed29991dd97e916c7b11a5ccf944d44b5
nigel-riss <mr.kurenkov@gmail.com>
2016-11-23 17:01:53 +0200

    Hero and buttons v.0.0.1

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    sassModuleImporter = require('sass-module-importer');
+
+// Sass task
+gulp.task('sass', function() {
+    return gulp.src('./src/assets/sass/**/*.scss')
+        .pipe(sass({ importer: sassModuleImporter() }))
+        .on('error', function(errorInfo) {
+            console.log(errorInfo.toString());
+            this.emit('end');
+        })
+        .pipe(gulp.dest('./src/assets/css/'));
+});
---
+++ gulp/tasks/watch.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    browserSync = require('browser-sync');
+
+// CSSInject task
+gulp.task('cssInject', ['sass'], function() {
+    return gulp.src('./src/assets/css/styles.css')
+        .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function() {
+    browserSync.init({
+        server: {
+            baseDir: "src"
+        }
+    });
+
+    watch('./src/*.html', function() {
+        browserSync.reload();
+    });
+
+    watch('./src/assets/sass/**/*.scss', function() {
+        gulp.start('sass');
+    })
+
+    watch('./src/assets/css/**/*.css', function() {
+        console.log('sass works');
+        gulp.start('cssInject');
+    });
+});
